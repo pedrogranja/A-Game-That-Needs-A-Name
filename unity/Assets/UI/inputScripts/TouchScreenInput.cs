8d9da2b66258e4fd9160bc6f6b6a5c6f9095bef0
pedrogranja <pedrogranja013@hotmail.com>
2018-06-07 15:07:29 +0100

    touch screen input and simple grass tile placement script issue #3

---
+++ unity/Assets/UI/inputScripts/TouchScreenInput.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScreenInput : MonoBehaviour {
+
+    public LayerMask touchInputMask;
+    [SerializeField] private Camera camera;
+    private RaycastHit hit;
+    private GameObject prevGameObject;
+
+    // Update is called once per frame
+    void Update () {
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
+        {
+
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, touchInputMask))
+            {
+                GameObject recipient = hit.transform.gameObject;
+
+                if(prevGameObject != null && prevGameObject != recipient)
+                    prevGameObject.SendMessage("ObjectDeselected", hit.point, SendMessageOptions.DontRequireReceiver);
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+
+                prevGameObject = recipient;
+            }
+            
+        }
+#endif
+
+        if (Input.touchCount > 0)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                Ray ray = Camera.current.ScreenPointToRay(touch.position);
+
+                if(Physics.Raycast(ray, out hit, touchInputMask))
+                {
+                    GameObject recipient = hit.transform.gameObject;
+
+                    if (prevGameObject != null && prevGameObject != recipient)
+                        prevGameObject.SendMessage("ObjectDeselected", hit.point, SendMessageOptions.DontRequireReceiver);
+
+                    if (touch.phase == TouchPhase.Began)
+                    {
+                        recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+
+                    prevGameObject = recipient;
+                }
+            }
+        }
+		
+	}
+}
--- unity/Assets/gameobjects/items/Network.cs
+++ unity/Assets/gameobjects/items/Network.cs
@@ -9,10 +9,11 @@ public class Network : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        Application.targetFrameRate = 30;
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         updateNodes();
 	}
 
