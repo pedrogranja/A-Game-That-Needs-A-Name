8cded4924a032fb38a3a630ef48c5c619a2168b0
Diogo Tito Vitor Marques <fc47847@alunos.fc.ul.pt>
2018-07-15 13:13:58 +0100

    HexagonalTile now checks for neighbours above and below
    I also added the HexagonalTile script to all the tiles in mainScene to
    test it

--- unity/Assets/Systems/TileSystem/HexagonalTile.cs
+++ unity/Assets/Systems/TileSystem/HexagonalTile.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections.Generic;
 
 public class HexagonalTile : MonoBehaviour {
@@ -16,75 +17,71 @@ public class HexagonalTile : MonoBehaviour {
         public HexagonalTile down;
     }
 
-    private struct ArrowPointing
-    {
-        public Transform arrow;
-        public HexagonalTile target;
-
-        public ArrowPointing(Transform arrow, HexagonalTile target)
-        {
-            this.arrow = arrow;
-            this.target = target;
-        }
-    }
-
     public Neighbours neighbours;
-    public Transform arrowPrefab;
-
-    private List<ArrowPointing> arrowsToTiles = new List<ArrowPointing>(6);
 
     public void FindMyNeighbours(Neighbours neighbours)
     {
         float sqrt3 = Mathf.Sqrt(3);
 
-        neighbours.left = NearestTileTo(2 * Vector3.left);
-        neighbours.right = NearestTileTo(2 * Vector3.right);
+        neighbours.left = NearestTileTo(Vector3.left);
+        neighbours.right = NearestTileTo(Vector3.right);
         neighbours.upLeft = NearestTileTo(new Vector3(-1, 0, sqrt3));
         neighbours.downLeft = NearestTileTo(new Vector3(-1, 0, -sqrt3));
         neighbours.upRight = NearestTileTo(new Vector3(1, 0, sqrt3));
         neighbours.downRight = NearestTileTo(new Vector3(1, 0, -sqrt3));
+        neighbours.up = NearestTileTo(Vector3.up);
+        neighbours.down = NearestTileTo(Vector3.down);
+    }
+
+    /// <summary>
+    /// Prints a line in the form
+    /// "game object name         raycast direction      [tiles hit (distance)]"
+    /// </summary>
+    /// <param name="direction"></param>
+    /// <param name="hits"></param>
+    private void DebugRaycast(Vector3 direction, RaycastHit[] hits)
+    {
+        Func<Transform, double> dist = t => Vector3.Distance(transform.position, t.position);
+        //Func<string, string> pad = str => str + "<color=#b0b0b0>" + new string('_', Mathf.Max(0, 25 - str.Length)) + "</color>";
+        Func<string, string> pad = str => str + "<color=#dbdbdb>" + new string('_', Mathf.Max(0, 25 - str.Length)) + "</color>";
+
+        string hitsLog = System.Linq.Enumerable.Aggregate<RaycastHit, string>(
+            source: hits,
+            seed: "",
+            func: (acc, hit) => acc +
+                                (acc.Length == 0 ? "" : ", ") +
+                                hit.transform.name +
+                                " (<b>" +
+                                dist(hit.transform).ToString("F2") +
+                                "</b>)");
+
+        Debug.Log(pad(name) + pad(direction.ToString()) + "[" + hitsLog + "]");
+
+        Func<float, float> f = (x) => (x + 2) / 4;
+        Color arrowColor = new Color(f(direction.x), f(direction.y), f(direction.z));
+        Debug.DrawRay(transform.position, direction, arrowColor, 10, false);
     }
 
     private HexagonalTile NearestTileTo(Vector3 direction)
     {
-        direction.y -= 0.5f;
         RaycastHit[] hits = Physics.RaycastAll(transform.position, direction, 2.0f);
 
-        System.Func<float, float> f = (x) => (x + 2) / 4;
-        Color arrowColor = new Color(f(direction.x), 0, f(direction.z));
+        Func<RaycastHit, double> dist = hit => Vector3.Distance(transform.position, hit.transform.position);
+        Array.Sort<RaycastHit>(hits, (t1, t2) => (int) (1000000 * (dist(t1) - dist(t2))));
+
+        DebugRaycast(direction, hits);
 
         if (hits.Length > 0) {
             HexagonalTile result = hits[0].transform.GetComponentInParent<HexagonalTile>();
-            if (result == this && hits.Length > 1)
-                result = hits[1].transform.GetComponentInParent<HexagonalTile>();
-            if (result != this)
-            {
-                Transform arrow = Instantiate<Transform>(arrowPrefab, transform);
-                arrow.GetComponentInChildren<MeshRenderer>().material.color = arrowColor;
-                arrowsToTiles.Add(new ArrowPointing(arrow, result));
-                return result;
-            }
+            return result;
         }
         return null;
     }
 
     // Use this for initialization
     void Start () {
-        /*
         FindMyNeighbours(neighbours);
-        GetComponentInChildren<TextMesh>().text = name.Trim("GroundTilecvybt ()".ToCharArray());
-        */
 	}
 	
-    /*
-	// Update is called once per frame
-	void Update () {
-        foreach (ArrowPointing arrowToTile in arrowsToTiles)
-        {
-            float distance = Vector3.Distance(arrowToTile.arrow.position, arrowToTile.target.transform.position);
-            arrowToTile.arrow.localScale = new Vector3(distance, distance, distance);
-        }
-	}
-    */
 }
 
